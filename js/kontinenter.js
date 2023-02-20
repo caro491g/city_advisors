fd57b5087ef7eae82f64b336470ae9e8a0e8aad8
kaowsoy <kaowsoypoemsomboon@gmail.com>
2023-02-20 13:10:59 +0100

    url

--- js/kontinenter.js
+++ js/kontinenter.js
@@ -1,9 +1,12 @@
-fetch("https://projektrejse-c325.restdb.io/rest/cities", {
-  method: "get",
-  headers: {
-    "x-apikey": "63f32d1f478852088da68494",
-  },
-})
+fetch(
+  "https://projektrejse-c325.restdb.io/rest/cities?q={%22$distinct%22:%22kontinent%22}",
+  {
+    method: "get",
+    headers: {
+      "x-apikey": "63f32d1f478852088da68494",
+    },
+  }
+)
   .then((e) => e.json())
   .then(showCategories);
 
@@ -19,8 +22,8 @@ function showCategory(cat) {
   const clone = tempalte.cloneNode(true);
 
   //ændre indhold
-  clone.querySelector("a").textContent = cat.category;
-  clone.querySelector("a").href = `lande.html?category=${cat.category}`;
+  clone.querySelector("a").textContent = cat;
+  clone.querySelector("a").href = `lande.html?category=${cat}`;
   //appender
   document.querySelector(".category ol").appendChild(clone);
 }
