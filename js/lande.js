0374b91d691ee1fff904d21a5c925bcd0da26021
Mathias Olesen <math09s0@stud.kea.dk>
2023-02-20 11:04:29 +0100

    dynamisk_ikke færdig

---
+++ js/lande.js
@@ -0,0 +1,21 @@
+fetch("https://projektrejse-c325.restdb.io/rest/cities", {
+    method: "get",
+    headers: {
+        "x-apikey": "63f32d1f478852088da68494",
+    },
+    })
+    .then((e) => e.json())
+    .then(showCountries);
+
+    function showCountries(countries) {
+        countries.forEach(showCountry);
+      }
+      
+      function showCountry(cities) {
+        console.log(cities);
+        const template = document.querySelector(".lande").content;
+        const copy = template.cloneNode(true);
+        copy.querySelector("h2").textContent = cities.country;
+        copy.querySelector(".by_navn").textContent = cities.name;
+        document.querySelector("main").appendChild(copy);
+      }
